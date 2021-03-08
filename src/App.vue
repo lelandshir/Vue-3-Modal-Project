1e9a4bfced9dc9e5b355b334ebec1485f54796c7
lelandshir <lelandshir@gmail.com>
2021-03-07 22:04:59 -0500

    restarted project, first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>{{ title }}</h1>
+	<b>{{ creator }}</b>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	data() {
+		return {
+			title: "Vue App 1.0.0 ☺️",
+			creator: "Léland Shirley",
+		};
+	},
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app
+	font-family: Avenir, Helvetica, Arial, sans-serif
+	-webkit-font-smoothing: antialiased
+	-moz-osx-font-smoothing: grayscale
+	text-align: center
+	color: #2c3e50
+	margin-top: 60px
 </style>
